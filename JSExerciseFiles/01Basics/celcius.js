f9e88829ca1c170372634a083c031dc7f6a5bcb6
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-27 21:15:46 +0530

    basics
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

---
+++ JSExerciseFiles/01Basics/celcius.js
@@ -0,0 +1,10 @@
+var score =100;
+score =130;
+const bonus  =50;
+
+var finalScore = (score + bonus)*1.8;
+
+console.log(finalScore);
+
+
+let a  = (5 + (4 * 3))  ;
---
+++ JSExerciseFiles/01Basics/variables.js
@@ -0,0 +1,9 @@
+let name ='Puneet Kumar';
+console.log(name + typeof(name));
+
+let score =20;
+let bonus = "20";
+console.log(score + bonus);
+console.log(typeof(score + bonus));
+
+
